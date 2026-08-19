8f3236a9435c3d43c5d13a62c58da0fa4089dc5f
Awuacero <awuacero@gmail.com>
2026-08-19 09:56:59 +0200

    Update predict_multi_batch.py

--- predict_multi_batch.py
+++ predict_multi_batch.py
@@ -65,39 +65,41 @@ def batch_predict(data,arch_cnn,arch_caps_caps,model_cnn,model_caps,nclass,outpu
 
 class ProtIDResult(object):
     """docstring for ClassName"""
-    def __init__(self, prot_id, scores=list(), residues=list(), positions=list(), ptmtypes=list()):
+    def __init__(self, prot_id, cutoff_val=0.65, scores=None, residues=None, positions=None, ptmtypes=None):
         super(ProtIDResult, self).__init__()
         self.prot_id = prot_id
-        self.residues_dic = {prot_id:{}} #only one per postion!
-        self.scores_dic = {prot_id:{}} #all the following three use prot_id and position as key 
+        self.cutoff = cutoff_val # Guarda el cutoff
+        self.residues_dic = {prot_id:{}} 
+        self.scores_dic = {prot_id:{}}  
         self.ptmtypes_dic = {prot_id:{}}
+        
+        if scores is None: scores = []
+        if residues is None: residues = []
+        if positions is None: positions = []
+        if ptmtypes is None: ptmtypes = []
     
     def update(self, prot_id, scores, residues, positions, ptmtypes):
         if positions not in self.ptmtypes_dic[prot_id]:
-            self.ptmtypes_dic[prot_id][positions] = [ptmtypes] #arrary to keep the scores,residues and ptmtypes in the same order.
-        elif ptmtypes in self.ptmtypes_dic[prot_id][positions]: # will never occur! no duplicate ptm types for same prot_id and same position
+            self.ptmtypes_dic[prot_id][positions] = [ptmtypes] 
+        elif ptmtypes in self.ptmtypes_dic[prot_id][positions]: 
             print("duplicate resulte!\n")
             return
         else:self.ptmtypes_dic[prot_id][positions] += [ptmtypes]
         
         if positions not in self.scores_dic[prot_id]:
             self.scores_dic[prot_id][positions] = [round(scores,3)] 
-        else:self.scores_dic[prot_id][positions] += [round(scores,3)] #add new score for this position
+        else:self.scores_dic[prot_id][positions] += [round(scores,3)] 
         
-        if positions not in self.residues_dic[prot_id]: # 
-            self.residues_dic[prot_id][positions]  = residues #only one residue shown for one position!
+        if positions not in self.residues_dic[prot_id]: 
+            self.residues_dic[prot_id][positions]  = residues 
     
     def __str__(self):
         res_str = ""
-        defaultcutoff=0.65;
+        # defaultcutoff=0.65; <--- BORRADO PARA USAR EL PARÁMETRO
         res_str = res_str+self.prot_id+"\n"
-        for pos in sorted(self.scores_dic[self.prot_id].keys()): #order by pos, it is the key for scores_dic[prot_id]
-            #res_str = res_str+"\""+self.prot_id+"\"\t"
+        for pos in sorted(self.scores_dic[self.prot_id].keys()): 
             res_str = res_str+str(pos)+"\t"
-            #res_str = res_str+str(','.join(self.residues_dic[self.prot_id][pos]))+"\t"
-            res_str = res_str+str(self.residues_dic[self.prot_id][pos])+"\t" # no need to add \"
-            #res_str = res_str+str(','.join([str(x) for x in self.scores_dic[self.prot_id][pos]]))+"\t"
-            #res_str = res_str+str(','.join(self.ptmtypes_dic[self.prot_id][pos]))+"\t"
+            res_str = res_str+str(self.residues_dic[self.prot_id][pos])+"\t" 
             ptms=[]
             pastptms=[]
             for index in range(len(self.scores_dic[self.prot_id][pos])):
@@ -110,7 +112,8 @@ class ProtIDResult(object):
                     ptmln=self.ptmtypes_dic[self.prot_id][pos][index]+":"+str(self.scores_dic[self.prot_id][pos][index])
                 
                 ptms.append(ptmln)
-                if self.scores_dic[self.prot_id][pos][index] > defaultcutoff:
+                # SE UTILIZA EL CUTOFF PROPORCIONADO POR EL USUARIO
+                if self.scores_dic[self.prot_id][pos][index] > self.cutoff:
                        pastptms.append(ptmln)
             
             res_str+=';'.join(ptms)+"\t"
@@ -127,19 +130,24 @@ if __name__ == "__main__":
     parser.add_argument('-input',  dest='inputfile', type=str, help='Protein sequences to be predicted in FASTA format.', required=True)
     parser.add_argument('-output',  dest='output', type=str, help='prefix of the prediction results.', required=True)
     parser.add_argument('-model-prefix',  dest='modelprefix', type=str, help='prefix of custom model used for prediciton. If donnot have one, please run train_capsnet_10fold_ensemble.py and train_CNN_10fold_ensemble to train models for a particular PTM type.', required=False,default=None)
+    
+    # NUEVO ARGUMENTO: -cutoff
+    parser.add_argument('-cutoff', dest='cutoff', type=float, help='Prediction threshold.', required=False, default=0.65)
+    
     args = parser.parse_args()
     
-    ensure_dir(args.output) #mkfolders for outputfile
-    websiteoutput = open(args.output+"_predicted_num.txt",'w') #generate this file at the beginning! 
+    ensure_dir(args.output) 
+    websiteoutput = open(args.output+"_predicted_num.txt",'w') 
     websiteoutput.write("Start:0\n")
     websiteoutput.close()
+    
     if args.modelprefix is None:
        print("If you want to do prediction by a custom model, please specify the prefix for an existing custom model by -model-prefix!\n\
        It indicates two files [-model-prefix]_HDF5model and [-model-prefix]_parameters.\n \
        If you don't have such files, please run train_general.py or train_kinase.py to get the custom model first!\n"
        )
        exit()
-    else: #custom prediction
+    else: 
       foldername,filename = os.path.split(args.modelprefix)
       print(foldername)
       print(filename)
@@ -148,7 +156,7 @@ if __name__ == "__main__":
       num_ptms = len(modeloptins)
       prot_id_dic={}
       ptmindex=0
-      #if the window size is not fixed as 16, move this below window=int(parameters.split("\t")[1])
+      
       arch_caps = Capsnet_main(np.zeros([3,2*16+1,21]),[],nb_epoch=1,compiletimes=0,lr=0.001,batch_size=500,lam_recon=0,routings=3,modeltype='nogradientstop',nb_classes=2,predict=True)
       arch_cnn = MultiCNN(np.zeros([3,2*16+1,21]),[],nb_epoch=1,compiletimes=0,batch_size=500,nb_classes=2,predict=True)
       
@@ -174,25 +182,24 @@ if __name__ == "__main__":
         predictproba,y_true=batch_predict(testfrag,arch_cnn,arch_caps,model_cnn,model_caps,nclass,args.output,foldnum,num_ptms,ptmtype)           
         poses=poses+1;
         ptmindex+=1
-        #results=np.column_stack((ids,poses,focuses,predictproba))
-        #result=pd.DataFrame(results)
-        #result.to_csv(args.output+"_"+ptmtype+".txt", index=False, header=None, sep='\t',quoting=csv.QUOTE_NONNUMERIC)
+        
         print("Successfully predicted from model:"+ptmtype+"!\n")
         for i in range(len(ids)):
             prot_id=ids.values[i][0]
-            #print("prot_id is "+prot_id)
             if prot_id not in prot_id_dic:
-                    prot_id_dic[prot_id] = ProtIDResult(prot_id)
+                    # SE PASA EL CUTOFF A LA CLASE
+                    prot_id_dic[prot_id] = ProtIDResult(prot_id, cutoff_val=args.cutoff) 
             
             prot_id_dic[prot_id].update(prot_id, predictproba[i][0], focuses.values[i][0], poses.values[i][0], ptmtype)
         
       
-      #write results to file
       target = open(args.output+"_results.txt", "w")
-      #add a header
-      target.write("Position\tResidue\tPTMscores\tCutoff=0.65\n")
-      for prot_id in idlist: #must keep the order!
-          if prot_id in prot_id_dic.keys(): #only print proteins in the result files. some protein do results dont print them!
+      
+      # LA CABECERA AHORA REFLEJA EL CUTOFF USADO
+      target.write(f"Position\tResidue\tPTMscores\tCutoff={args.cutoff}\n") 
+      
+      for prot_id in idlist: 
+          if prot_id in prot_id_dic.keys(): 
                 v = str(prot_id_dic[prot_id])
                 target.write(v)
       
@@ -200,7 +207,3 @@ if __name__ == "__main__":
       websiteoutput = open(args.output+"_predicted_num.txt",'w')
       websiteoutput.write("All:100\n")
       websiteoutput.close()
-      
-
-#test in musiteDeepCapsnet
-#python3 predict_multi_batch.py -input test_SUMO.fasta -output test_SUMO_multipredict -model-prefix "./models//Ubiquitination-K|SUMOylation-K|N6-acetyllysine|Methyllysine-K"
